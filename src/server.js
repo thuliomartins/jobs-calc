b833d32763ede8f1a9e02e94ffbbfa78d6c66096
Thulio Barbosa <thulio.barbosa0@gmail.com>
2022-02-21 10:13:01 -0300

    Finalizando a segunda aula

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ server.set("view engine", "ejs");
 
 server.use(express.static("./public")); 
 
+server.use(express.urlencoded({ extended: true }));
+
 server.use(routes);
 
 server.listen(PORT, () => console.log("Start Server"));
